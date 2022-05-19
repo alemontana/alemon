9dd5879880eea4a1443fd2f22e91b487a4af7866
Alex Montana <alex.montana.bello@gmail.com>
2022-05-18 21:23:23 -0500

    init

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+ $fp = fopen("temperatura.txt", "r");
+ while (!feof($fp)){
+    $linea = fgets($fp);
+    echo $linea;
+ }
+ fclose($fp);
+ // return "hola"; //    return view('welcome');
+});
+
+Route::get('/temperature/{temperature}', function($temperature){
+  $file = fopen('temperatura.txt','a');
+  fwrite($file, date("H:i:s")." - ".$temperature."\n");
+  fclose($file);
+  //Storage::disk('local')->put(temp.txt, $temperature);
+});
